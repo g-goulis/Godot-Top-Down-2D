b1c4876f94cb582546ca081509ba0287b9a76ae1
Gabriel Goulis <ninjagabe2003@gmail.com>
2023-08-02 23:04:45 -0500

    Twin-stick movement, shadows

--- Player/CharacterBody2D.cs
+++ Player/CharacterBody2D.cs
@@ -10,16 +10,10 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
 	// public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 
-	private AnimationPlayer animationPlayer;
-	private AnimatedSprite2D animatedSprite2D;
-
-	private String idleDirection;
+	
 
 
 	public override void _Ready(){
-		animationPlayer = (AnimationPlayer) GetNode("AnimationPlayer");
-		animatedSprite2D = (AnimatedSprite2D) GetNode("AnimatedSprite2D");
-		idleDirection = "Idle_Down";
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -27,33 +21,17 @@ public partial class CharacterBody2D : Godot.CharacterBody2D
 		Vector2 velocity = Velocity;
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-		if(direction.X == 1 && animatedSprite2D.FlipH == false) {
-			animatedSprite2D.FlipH = true;
-		} else if(direction.X == -1 && animatedSprite2D.FlipH == true) {
-			animatedSprite2D.FlipH = false;
-		}
-
+		// Update player velocity
 		if (direction != Vector2.Zero) {
 			velocity.X = direction.X * Speed;
 			velocity.Y = direction.Y * Speed;
-
-			if(direction.X != 0) {
-				idleDirection = "Idle_Left";
-				animationPlayer.Play("Walk_Left");
-
-			} else if (direction.Y != 0) {
-				idleDirection = "Idle_Down";
-				animationPlayer.Play("Walk_Down");	
-
-			}
-
-
-
 		} else {
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-			animationPlayer.Play(idleDirection);
 		}
+
+		// Update player rotation
+		LookAt(GetGlobalMousePosition());
 
 		Velocity = velocity;
 		MoveAndSlide();
